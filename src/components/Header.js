7d369e9596ddcfbd99becf403ccea065391b1686
Punara Manodya <pmcmanodya94@gmail.com>
2025-12-02 14:48:42 +0530

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -70,7 +70,7 @@ const Header = () => {
           align-items: center;
           gap: 15px;
           animation: slideInLeft 1s ease-out;
-          transform: translateX(-110px);
+          transform: translateX(-260px);
 
         }
         
@@ -112,7 +112,7 @@ const Header = () => {
           align-items: center;
           gap: 20px;
           margin-left: 300px;
-           transform: translateX(260px);
+           transform: translateX(300px);
           
         }
         
