9d58fe20476aa2cc08eddfe971550f385f0ddcfd
Punara Manodya <pmcmanodya94@gmail.com>
2025-12-08 11:01:01 +0530

    add new style to welcom section

--- src/components/tabs/HelpDesk.js
+++ src/components/tabs/HelpDesk.js
@@ -161,14 +161,18 @@ const HelpDesk = () => {
   };
 
   return (
-    <div
-     className="tab-content active"
-     style={{
-        padding: '0px',
-        position: 'relative',
-        overflowX: 'hidden',
-        overflowY: 'hidden',
-     }}> 
+<div
+  className="tab-content active"
+  style={{
+    padding: 0,
+    position: 'relative',
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column"
+  }}>
+
 
       <div 
         className="welcome-section"
